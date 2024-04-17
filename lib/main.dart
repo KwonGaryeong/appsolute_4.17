e22ffa5e1d11fa04473c2322b7ec932e3e0e23dc
oieyli <gryulita0518@naver.com>
2024-04-17 20:21:38 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:appsolute2/screens/first_screen.dart';
+import 'package:appsolute2/screens/second_screen.dart';
+import 'package:appsolute2/screens/third_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title:'My App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState(){
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('My APP'),
+          bottom: TabBar(
+            controller: _tabController,
+            tabs: [
+              Tab(
+                text: 'Tab1',
+              ),
+              Tab(
+                text: 'Tab2',
+              ),
+              Tab(
+                text: 'Tab3',
+              ),
+            ],
+          ),
+        ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          Center(
+            child: ElevatedButton(
+              onPressed: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => FirstScreen()),
+                );
+              },
+              child: Text(
+              'Use Stack'
+              ),
+            ),
+          ),
+          Center(
+            child: ElevatedButton(
+              onPressed: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SecondScreen()),
+                );
+              },
+              child: Text(
+                  'Use image'
+              ),
+            ),
+          ),
+          Center(
+            child: Text(
+                'Tab3'
+            ),
+          ),
+        ],
+      ),
+      );
+    }
+  }
+
+
